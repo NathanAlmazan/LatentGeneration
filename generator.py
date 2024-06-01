4bfc6adcda76e645a177c7f1595c1157607b0d4b
NathanAlmazan <nathanael.almazan@gmail.com>
2024-06-01 08:49:01 +0800

    added flask api

--- generator.py
+++ generator.py
@@ -4,12 +4,41 @@ import torch
 import numpy as np
 
 from PIL import Image
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+
 from sampler import KLMSSampler
 from diffusion import Diffusion
 from autoencoder import Decoder
 
+# load vocabulary
+stemmer = PorterStemmer()
+stop_words = set(stopwords.words('english'))
+# load tokens
+with open(os.path.join("checkpoints", "labels.txt"), "r") as file:
+    labels = file.readlines()
+    labels = [label.strip().lower() for label in labels]
+    labels = [set([stemmer.stem(word) for word in word_tokenize(label) if word not in stop_words]) for label in labels]
 
-def generate(labels: list[int]):
+
+def tokenize(prompt: str, count=8):
+    prompt = set([stemmer.stem(word) for word in word_tokenize(prompt.lower()) if word not in stop_words])
+    # find tokens
+    results = []
+    for idx, label in enumerate(labels):
+        if len(label.intersection(prompt)) > 0:
+            results.append(idx)
+    # repeat tokens to fit number of expected results
+    if len(results) < count:
+        repeat = (count // len(results)) + 1
+        results = results * repeat
+    results = results[:count]
+    
+    return results
+
+
+def generate_images(labels: list[int]):
     # get label embedding
     embeddings = np.load(os.path.join('checkpoints', 'embeddings.npy'), allow_pickle=True)
     embeddings = torch.from_numpy(embeddings[labels])
---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+from generator import tokenize, generate_images
+from flask import Flask, request, jsonify, send_from_directory
+
+
+app = Flask(__name__)
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    body = request.json
+    
+    if body is None:
+        return jsonify({'error': 'Invalid request body'}), 400
+    
+    prompt = body['prompt']
+    
+    if not prompt or not isinstance(prompt, str):
+        return jsonify({'error': 'Prompt must be a non-empty string'}), 400
+    
+    # tokenize prompt
+    labels = tokenize(prompt)
+    images = generate_images(labels)
+    
+    return jsonify({'image_urls': [ f"https://imagine.automos.net/generated/{image}" for image in images ]})
+
+
+@app.route('/generated/<path:filename>')
+def serve_image(filename):
+    return send_from_directory('generated', filename)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=False)
