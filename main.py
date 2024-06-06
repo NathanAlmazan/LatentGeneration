3e2d5c11d97b5716f46d9799131e73130f7239ae
NathanAlmazan <nathanael.almazan@gmail.com>
2024-06-06 12:44:36 +0800

    added label endpoint

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
 from generator import tokenize, generate_images
 from flask import Flask, request, jsonify, send_from_directory
 
-
 app = Flask(__name__)
 
-@app.route('/generate', methods=['POST'])
-def generate():
+
+@app.route('/prompt/generate', methods=['POST'])
+def prompt_generate():
     try:
         body = request.json
-    
+
         if body is None:
             return jsonify({'error': 'Invalid request body'}), 400
-        
+
         prompt = body['prompt']
         count = body['count'] if isinstance(body['count'], int) else 8
-        
+
         if not prompt or not isinstance(prompt, str):
             return jsonify({'error': 'Prompt must be a non-empty string'}), 400
-        
+
         if count < 2 or count > 12:
             return jsonify({'error': 'Count must be an integer between 2 and 12'}), 400
-        
+
         # tokenize prompt
         labels = tokenize(prompt, count)
-        
+
         if len(labels) == 0:
             return jsonify({'error': 'Invalid prompt.'}), 400
-        
+
         images = generate_images(labels)
-        
-        return jsonify({'image_urls': [ f"https://imagine.automos.net/generated/{image}" for image in images ]})
+
+        return jsonify({'image_urls': [f"https://imagine.automos.net/generated/{image}" for image in images]})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/label/generate', methods=['POST'])
+def label_generate():
+    try:
+        body = request.json
+
+        if body is None:
+            return jsonify({'error': 'Invalid request body'}), 400
+
+        labels = body['labels']
+
+        if len(labels) == 0:
+            return jsonify({'error': 'Invalid labels.'}), 400
+
+        if len([label for label in labels if label < 0 or label > 500]) > 0:
+            return jsonify({'error': 'Invalid label.'}), 400
+
+        images = generate_images(labels)
+
+        return jsonify({'image_urls': [f"https://imagine.automos.net/generated/{image}" for image in images]})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
